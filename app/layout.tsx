df91467818e48170c2cd25c870c42ab36a7ef86f
sidedishjob <private0102iwa@gmail.com>
2025-04-21 08:01:24 +0900

    fix: layout.tsx に Head.tsx を読み込むことで favicon が正しく反映されるよう修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import Providers from '@/components/Providers';
 import AppLayout from '@/layouts/AppLayout';
+import Head from './Head';
 
 export const metadata = {
 	title: {
@@ -13,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="ja" suppressHydrationWarning>
+			<Head />
 			<body className="bg-white text-black dark:bg-zinc-900 dark:text-white transition-colors">
 				<Providers>
 					<AppLayout>{children}</AppLayout>
