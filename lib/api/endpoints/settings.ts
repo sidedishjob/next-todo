d320eaa0dc0b767d733117e7b8e0bfeee007d270
sidedishjob <private0102iwa@gmail.com>
2025-04-28 17:46:34 +0900

    refactor: settingsの戻り値をapiCallerからdataのみ抽出する形に修正

--- lib/api/endpoints/settings.ts
+++ lib/api/endpoints/settings.ts
@@ -6,14 +6,15 @@ import { API_ROUTES } from '../routes';
  * テーマを取得するAPI
  */
 export const getTheme = async (): Promise<Setting> => {
-	return await apiCaller<Setting>('GET', API_ROUTES.settings.theme);
+	const { data } = await apiCaller<Setting>('GET', API_ROUTES.settings.theme);
+	return data;
 };
 
 /**
  * テーマを更新するAPI（成功したら true）
  */
 export const updateTheme = async (theme: 'light' | 'dark'): Promise<void> => {
-	return await apiCaller<void>('POST', API_ROUTES.settings.theme, {
+	await apiCaller<void>('POST', API_ROUTES.settings.theme, {
 		theme,
 	});
 };
