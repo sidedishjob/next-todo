02cdd9539855531d9ca5841ca5fbbffad7c7c82f
sidedishjob <private0102iwa@gmail.com>
2025-04-29 07:43:29 +0900

    chore: クラス結合ユーティリティ cn を微修正

--- lib/utils/cn.ts
+++
@@ -1,8 +0,0 @@
-/**
- * クラス名を結合するためのユーティリティ関数
- * @param  {...string} classes - 結合するクラス名
- * @returns {string} 結合されたクラス名
- */
-export function cn(...classes: (string | false | null | undefined)[]): string {
-	return classes.filter(Boolean).join(' ');
-}
